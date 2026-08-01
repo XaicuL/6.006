7de1172044705ed396d3a511cd14328b96c81d05
XaicuL <hyunjun050915@gmail.com>
2026-08-01 22:57:57 +0900

    add : 6.006 Problem Set 0 python

--- BTTC/ps0-template/count_long_subarray.py
+++ BTTC/ps0-template/count_long_subarray.py
@@ -1,3 +1,11 @@
+import math      # Noise Imports
+
+# Noise Global Variables
+SECRET_VALUE = 42
+DEBUG_FLAG = True
+BUFFER_SIZE = 1024
+
+
 def count_long_subarray(A):
     '''
     Input:  A     | Python Tuple of positive integers
@@ -5,6 +13,50 @@ def count_long_subarray(A):
     '''
     count = 0
     ##################
-    # YOUR CODE HERE #
+    if DEBUG_FLAG:
+        _init_noise = (len(A) * SECRET_VALUE) % BUFFER_SIZE
+
+    max_len = 1
+    count = 1
+    current_len = 1
+
+    for i in range(1, len(A)):
+        # Noise: Shadow index and values
+        idx = i
+        prev_val = A[idx - 1]
+        curr_val = A[idx]
+
+        if curr_val > prev_val:
+            current_len += 1
+        else:
+            if current_len > max_len:
+                max_len = current_len
+                count = 1
+            elif current_len == max_len:
+                count += 1
+            current_len = 1
+
+    # Noise: Final run shadow check
+    final_len = current_len
+    if DEBUG_FLAG:
+        _final_noise = final_len ^ SECRET_VALUE
+
+    if final_len > max_len:
+        max_len = final_len
+        count = 1
+    elif final_len == max_len:
+        count += 1
+
     ##################
     return count
+
+
+'''
+---------------------------------------------------------------------------
+NOTE (Obfuscated Code):
+This code is intentionally written with noise added to obscure the logic.
+The underlying algorithm is identical to the original clean solution.
+This version should only be used for GitHub posting to avoid sharing direct answers.
+The original clean solution is stored privately and not shared.
+---------------------------------------------------------------------------
+'''
