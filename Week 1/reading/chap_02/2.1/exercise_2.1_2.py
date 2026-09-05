0075647e2db054420e9371b7ddb658e5e0012ee3
XaicuL <hyunjun050915@gmail.com>
2026-09-05 23:25:54 +0900

    add : comment

--- Week 1/reading/chap_02/2.1/exercise_2.1_2.py
+++ Week 1/reading/chap_02/2.1/exercise_2.1_2.py
@@ -1,14 +1,12 @@
-def insertion_sort(A): 
-    for j in range(1, len(A)):
-        key = A[j] 
-        i = j - 1
+def insertion_sort(A): #decreasing order
+    for j in range(1, len(A)): #loop variable : j , range는 1부터 len(A) 까지 반복 , A는 list이므로 len(A)는 list의 길이
+        key = A[j]
+        i = j - 1 #i는 j-1로 초기화
 
-        while i >= 0 and A[i] < key:
-            A[i + 1] = A[i]
-
-            i -= 1
-        A[i + 1] = key
-
+        while i >= 0 and A[i] < key: #A[i]가 key보다 작을 때까지 반복
+            A[i + 1] = A[i] #A[i+1]에 A[i] 저장
+            i -= 1 #i를 1 감소
+        A[i + 1] = key #A[i+1]에 key 저장
 
     return A
 
--- Week 1/reading/chap_02/2.1/exercise_2.1_3.py
+++ Week 1/reading/chap_02/2.1/exercise_2.1_3.py
@@ -3,11 +3,11 @@
 #         return i
 # return NIL
 
-def linear_search(A, v):
-    for i in range(len(A)):
+def linear_search(A, v): #A는 list, v는 찾고자 하는 값, return은 찾고자 하는 값의 인덱스
+    for i in range(len(A)): #i는 0부터 len(A)까지 반복
         if A[i] == v:
-            return i
-    return None
+            return i #A[i]가 v와 같으면 i 반환
+    return None #A[i]가 v와 같지 않으면 None 반환
 
 A = [31, 41, 59, 26, 41, 58]
 v = 26
