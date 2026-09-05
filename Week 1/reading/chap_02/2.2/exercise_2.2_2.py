93f3d6b2030b95d70bd67a0fa69b7af0b4353ec0
XaicuL <hyunjun050915@gmail.com>
2026-09-05 23:26:09 +0900

    add : comment

--- Week 1/reading/chap_02/2.2/exercise_2.2_2.py
+++ Week 1/reading/chap_02/2.2/exercise_2.2_2.py
@@ -7,12 +7,12 @@
 
 A = [3, 1, 4, 2, 5, 9]
 
-for i in range(len(A)-1):
+for i in range(len(A)-1): #i는 0부터 len(A)-1까지 반복
     min = i
-    for j in range(i+1, len(A)):
-        if A[j] < A[min]:
-            min = j
-    A[i], A[min] = A[min], A[i]
+    for j in range(i+1, len(A)): #j는 i+1부터 len(A)까지 반복
+        if A[j] < A[min]: #A[j]가 A[min]보다 작으면
+            min = j #min을 j로 변경
+    A[i], A[min] = A[min], A[i] #A[i]와 A[min]을 교환
 
 print(A)
 
